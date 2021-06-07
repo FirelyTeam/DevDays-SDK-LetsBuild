b9c1271c74c1a55304bc54c15aebcc720f8e2a66
Mirjam Baltus <mirjam@fire.ly>
2021-06-07 12:35:41 +0200

    Fleshed out code for mapping and displaying data

--- LetsBuild2021June/Mapper.cs
+++ LetsBuild2021June/Mapper.cs
@@ -14,6 +14,16 @@ namespace LetsBuild2021June
 
             patient.Id = record.PATIENT_ID;
 
+            // Check your data first, and change this code if you have more options or null values for gender
+            patient.Gender = (record.PATIENT_GENDER == "F") ? AdministrativeGender.Female : AdministrativeGender.Male;
+
+            patient.Name.Add(new HumanName().WithGiven(record.PATIENT_GIVENNAME).AndFamily(record.PATIENT_FAMILYNAME));
+            // This could also be accomplised without using fluent methods:
+            //var name = new HumanName();
+            //name.GivenElement.Add(new FhirString(record.PATIENT_GIVENNAME)); // given names go into a list
+            //name.Family = record.PATIENT_FAMILYNAME;
+            //patient.Name.Add(name);
+
             return patient;
         }
 
@@ -26,6 +36,22 @@ namespace LetsBuild2021June
             // Unit Code:   10*3/uL
 
             Observation wbc = new Observation();
+
+            wbc.Code = new CodeableConcept("http://loinc.org", "6690-2", "Leukocytes [#/volume] in Blood by Automated count");
+
+            wbc.Value = new Quantity(decimal.Parse(record.WBC), "10*3/uL");
+
+            wbc.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            wbc.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            wbc.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            wbc.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            wbc.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
 
             return wbc;
         }
@@ -40,6 +66,22 @@ namespace LetsBuild2021June
 
             Observation rbc = new Observation();
 
+            rbc.Code = new CodeableConcept("http://loinc.org", "789-8", "Erythrocytes [#/volume] in Blood by Automated count");
+
+            rbc.Value = new Quantity(decimal.Parse(record.RBC), "10*6/uL");
+
+            rbc.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            rbc.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            rbc.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            rbc.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            rbc.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
+
             return rbc;
         }
 
@@ -52,6 +94,22 @@ namespace LetsBuild2021June
             // Unit Code:   g/dL
 
             Observation hb = new Observation();
+
+            hb.Code = new CodeableConcept("http://loinc.org", "718-7", "Hemoglobin [Mass/volume] in Blood");
+
+            hb.Value = new Quantity(decimal.Parse(record.HB), "g/dL");
+
+            hb.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            hb.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            hb.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            hb.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            hb.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
 
             return hb;
         }
