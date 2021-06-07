fad5c143b16c2c005af4d101df7882f1c0086185
Mirjam Baltus <mirjam@fire.ly>
2021-06-07 11:45:31 +0200

    Adding sample solution session 1

---
+++ LetsBuild2021June/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LetsBuild2021June
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
