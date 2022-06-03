22536bbe7f2d5ad746a426a7186bf636309d0f30
Alexander Zautke <alexander@fire.ly>
2022-06-03 10:24:25 -0400

    Upgrade to new serializers

--- LetsBuild-SDK-LetsBuild/Program.cs
+++ LetsBuild-SDK-LetsBuild/Program.cs
@@ -5,6 +5,7 @@ using Hl7.Fhir.Specification.Source;
 using Hl7.Fhir.Validation;
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace LetsBuild
 {
@@ -22,7 +23,7 @@ namespace LetsBuild
         public static void Main(string[] args)
         {
             // pretty print the json
-            var jsonSerializationSettings = new FhirJsonSerializationSettings { Pretty = true };
+            var options = new JsonSerializerOptions(){WriteIndented = true}.ForFhir(typeof(Patient).Assembly);
 
             // Set up resolver
             var resolver = new CachedResolver(new MultiResolver(
@@ -38,7 +39,7 @@ namespace LetsBuild
             var outcome = validator.Validate(_patient);
 
             // print the outcome
-            Console.WriteLine($"Success: {outcome.Success} \n{outcome.ToJson(jsonSerializationSettings)}");
+            Console.WriteLine($"Success: {outcome.Success} \n{JsonSerializer.Serialize<OperationOutcome>(outcome, options)}");
 
 
             var client = new FhirClient("https://server.fire.ly/r4");
@@ -50,7 +51,7 @@ namespace LetsBuild
             try
             {
                 var oo = client.ValidateResource(_patient);
-                Console.WriteLine(oo.ToJson(jsonSerializationSettings));
+                Console.WriteLine(JsonSerializer.Serialize<OperationOutcome>(oo, options));
 
                 var pat = client.Create<Patient>(_patient);
 
@@ -58,15 +59,15 @@ namespace LetsBuild
 
                 // retrieve the patient again from the server:
                 var patFromServer = client.Read<Patient>($"Patient/{pat.Id}"); // or Read<Patient>(ResourceIdentity.Build("Patient", pat.Id));
-                Console.WriteLine($"Read patient from server with id : {pat.Id}\n{patFromServer.ToJson(jsonSerializationSettings)}");
+                Console.WriteLine($"Read patient from server with id : {pat.Id}\n{JsonSerializer.Serialize<Patient>(patFromServer, options)}");
 
                 // search functions:
                 Bundle results = client.Search<Patient>(new string[] { "family:exact=Visser" });
-                Console.WriteLine($"Search patient with family name exact to 'Visser'\n{results.ToJson(jsonSerializationSettings)}");
+                Console.WriteLine($"Search patient with family name exact to 'Visser'\n{JsonSerializer.Serialize<Bundle>(results, options)}");
 
                 // search by id:
                 results = client.SearchById<Patient>(pat.Id);
-                Console.WriteLine($"Search patient by Id\n{results.ToJson(jsonSerializationSettings)}");
+                Console.WriteLine($"Search patient by Id\n{JsonSerializer.Serialize<Bundle>(results, options)}");
 
                 // using the class SearchParams
                 var q = new SearchParams()
@@ -75,12 +76,12 @@ namespace LetsBuild
                     .SummaryOnly().Include("Patient:organization")
                     .LimitTo(5);
                 results = client.Search<Patient>(q);
-                Console.WriteLine($"Complex search\n{results.ToJson(jsonSerializationSettings)}");
+                Console.WriteLine($"Complex search\n{JsonSerializer.Serialize<Bundle>(results, options)}");
 
                 // continue with the next page:
                 results = client.Continue(results);
                 if(results is {})
-                    Console.WriteLine($"Next page\n{results.ToJson(jsonSerializationSettings)}");
+                    Console.WriteLine($"Next page\n{JsonSerializer.Serialize<Bundle>(results, options)}");
                 else
                     Console.WriteLine("No next page available");
             }
